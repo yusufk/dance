1cc4818053b5246442bbc316eec8d6cadeae69b1
Yusuf Kaka <yusuf.kaka@mtn.com>
2021-08-24 20:00:05 +0200

    mp4 codec

--- js/main.js
+++ js/main.js
@@ -124,6 +124,9 @@ function handleDataAvailable(event) {
 
 function getSupportedMimeTypes() {
   const possibleTypes = [
+    'video/mp4;codecs=h264,aac',
+    'video/mp4;codecs=h264,mp3',
+    'video/mp4;codecs=h264,vorbis',
     'video/webm;codecs=vp9,opus',
     'video/webm;codecs=vp8,opus',
     'video/webm;codecs=h264,opus',
